0bdb21ad5ad789840c7ec9f7e4d1b2dee381f8d2
Charles Huang <chuang93@gmail.com>
2018-12-30 22:38:08 -0500

    Initial commit

---
+++ src/main/java/com/phage/services/resources/IndexResource.java
@@ -0,0 +1,15 @@
+package com.phage.services.resources;
+
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//for use of the '/' base index URL of the services
+@RestController
+public class IndexResource {
+
+    @RequestMapping( value = "/", method = RequestMethod.GET)
+    public String Index(){
+        return "index resource";
+    }
+}
---
+++ src/main/java/com/phage/services/resources/LoginResource.java
@@ -0,0 +1,63 @@
+package com.phage.services.resources;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+
+@RestController
+public class LoginResource{
+
+
+    @RequestMapping(value = "/verify", method = RequestMethod.POST)
+    public String verifyToken(String idTokenString) throws GeneralSecurityException, IOException {
+        //TO DO:RESEARCH WHY IT IS RECOMENDED AND HOW TO MAKE TRANSPORT GLOBAL
+        HttpTransport transport = GoogleNetHttpTransport.newTrustedTransport();
+        JsonFactory jsonFactory = new JacksonFactory();
+
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
+                // Specify the CLIENT_ID of the app that accesses the backend:
+                .setAudience(Collections.singletonList("474339377424-mufjbd5juv939iukdqpa09m15uudmd3b.apps.googleusercontent.com"))
+                // Or, if multiple clients access the backend:
+                //.setAudience(Arrays.asList(CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3))
+                .build();
+
+                // (Receive idTokenString by HTTPS POST)
+
+        GoogleIdToken idToken = verifier.verify(idTokenString);
+        if (idToken != null) {
+            Payload payload = idToken.getPayload();
+
+            // Print user identifier
+            String userId = payload.getSubject();
+            System.out.println("User ID: " + userId);
+
+            // Get profile information from payload
+            String email = payload.getEmail();
+            boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
+            String name = (String) payload.get("name");
+            String pictureUrl = (String) payload.get("picture");
+            String locale = (String) payload.get("locale");
+            String familyName = (String) payload.get("family_name");
+            String givenName = (String) payload.get("given_name");
+
+            // Use or store profile information
+            // ...
+            if(emailVerified)
+                return "Email Verified";
+        } else {
+            return "Invalid ID token.";
+        }
+        return "Unable to verify ID: " + idTokenString;
+    }
+}
